bdc85a533aff7ed1ce946e53135162d22ae74ed0
MiguelRamirezMejia <miguelramirezcoach@gmail.com>
2023-01-15 22:15:29 -0500

    update all files section footer and header

--- templates/header.php
+++ templates/header.php
@@ -22,7 +22,7 @@
                 <a class="nav-link active" href="#" aria-current="page">Sistema <span class="visually-hidden">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Empleados</a>
+                <a class="nav-link" href="secciones/empleados/">Empleados</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Puestos</a>
