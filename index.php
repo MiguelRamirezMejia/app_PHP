e041ebf333777f9400d093892ebb17e35a91ae7f
MiguelRamirezMejia <miguelramirezcoach@gmail.com>
2023-01-15 21:55:17 -0500

    create footer and header templates

--- index.php
+++ index.php
@@ -1,43 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <title>APP PHP</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-</head>
-
-<body>
-  <header>
-    <!-- place navbar here -->
-    <nav class="navbar navbar-expand navbar-light bg-light">
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="#" aria-current="page">Sistema <span class="visually-hidden">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Empleados</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Puestos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Usuarios</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Cerrar sesión</a>
-            </li>
-        </ul>
-    </nav>
-  </header>
-
-  <main class="container">
+<?php include("templates/header.php"); ?>
     <br>
     <div class="p-5 mb-4 bg-light rounded-3">
         <div class="container-fluid py-5">
@@ -47,19 +8,5 @@
         </div>
       </div>
 
-  </main>
-  <footer>
-    <!-- place footer here -->
-    Desarrollado por Miguel Angel Ramírez
-  </footer>
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
-</body>
-
-</html>
+      <?php include("templates/footer.php"); ?>
+  
